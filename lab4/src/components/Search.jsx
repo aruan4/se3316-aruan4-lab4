f0972e7a59a64ac4701dba2b555fa80474f9f7ca
aruan4 <aruan4@uwo.ca>
2023-11-24 09:24:38 -0500

    working on search functionality

--- lab4/src/components/Search.jsx
+++ lab4/src/components/Search.jsx
@@ -1,20 +1,27 @@
-import {React, useRef, useState} from 'react';
+import {React, useState} from 'react';
 
 let Search = () => {
     //DOM
     //Hero search
-    const searchBar = useRef(null);
-    const searchResults = useRef(null);
+    const [name, updateName] = useState('');
+    let handleName = (event) => {
+        updateName(event.target.value);
+    }
+
     const [heroes, updateHeroes] = useState([]);
 
-    let fieldSearch = (content) => {
-        const newHero = content;
-        updateHeroes([...heroes, newHero]);
-        // fetch('api/superhero_info/search/:name')
-        // .then(res => res.json())
-        // .then(data => {
-            
-        }
+    let nameSearch = async () => {
+        await fetch(`api/superhero_info/search/${name}`)
+        // .then(res => {alert(res.json())})
+        .then(data => {
+            alert(data);
+            for(let property in data){
+                let newHero = `${data[property].name}\n${data[property].publisher}`
+                //Call update heroes ...heroes gets all elements in previous array, adds 2nd parameter
+                updateHeroes([...heroes, newHero]);
+            }
+        })
+    }
 
     return(
         <div className='px-16 py-12 m-8 bg-[#242323]'>
@@ -23,35 +30,35 @@ let Search = () => {
                 <div id='col1' className='grid grid-rows-5 justify-center'>
                     <p className='text-white'>Choose search options:</p>
                     <div>
-                        <input type="checkbox" className='m-2' defaultChecked={true}></input>
+                        <input type="checkbox" value='name' className='m-2' defaultChecked={true}></input>
                         <label className='text-white'>Name</label>
                     </div>
                     <div>
-                        <input type="checkbox" className='m-2'></input>
+                        <input type="checkbox" value='race' className='m-2'></input>
                         <label className='text-white'>Race</label>
                     </div>
                     <div>
-                        <input type="checkbox" className='m-2'></input>
+                        <input type="checkbox" value='publisher' className='m-2'></input>
                         <label className='text-white'>Publisher</label>
                     </div>
                     <div>
-                        <input type="checkbox" className='m-2'></input>
+                        <input type="checkbox" value='powers' className='m-2'></input>
                         <label className='text-white'>Powers</label>
                     </div>
                 </div>
                 <div id='col2' className='grid grid-cols-2 justify-center items-center'>
                     <div id='innerCol1' className='mx-2'>
-                        <input ref={searchBar} className='h-[25%] bg-[#242323] w-full rounded-md placeholder-white text-white' placeholder='Search content'></input>
+                        <input value={name} onChange={handleName} className='h-[25%] bg-[#242323] w-full rounded-md placeholder-white text-white' placeholder='Search content'></input>
                         <hr className='bg-white border-1 border-white'></hr>
                     </div>
                     <div id='innerCol2' className='mx-2 flex items-center justify-center'>
-                        <button onClick={() => fieldSearch('hello')}
+                        <button onClick={nameSearch}
                         className='bg-[#095a1f] hover:bg-[#107b2d] w-[100px] rounded-md font-small font-techFont my-6 mx-2 py-3 px-6 text-white'>Search</button>
                     </div>
                 </div>
                 <div id='col3' className='grid grid-rows-2 font-techFont bg-[#0e7f2c] m-6 rounded-xl drop-shadow-lg'>
                     <div id='innerCol1' className='flex items-center justify-center'>
-                        <ul ref={searchResults} className='text-white font-techFont'>
+                        <ul className='text-white font-techFont'>
                             {heroes.map((hero, index) => (
                                 <li className='p-2' key={index}>{hero}</li>
                             ))}
